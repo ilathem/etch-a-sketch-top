b8969e3c7a811b6517ad4dc9fadbfe4f88e3e49e
ilathem <isaiahlathem@gmail.com>
2023-11-30 11:00:08 -0600

    Create 16x16 grid

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const rootDiv = document.querySelector('#root');
+const rows = new Array(16);
+const grid = new Array(16);
+for (let i = 0; i < 16; i++) {
+    grid[i] = new Array(16);
+    rows[i] = document.createElement('div');
+    rows[i].classList.add('rowDiv');
+    for (let j = 0; j < 16; j++) {
+        grid[i][j] = document.createElement('div')
+        grid[i][j].classList.add('gridDiv');
+        rows[i].appendChild(grid[i][j]);
+    }
+    rootDiv.appendChild(rows[i]);
+}
